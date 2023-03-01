4ca81d709a635105c257d3d80e3f7689460f0d0b
MauricioFernanddes <mauriciojrfs@hotmail.com>
2023-03-01 03:34:58 -0300

    requesito 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando o componente Not Found', () => {
+  it('A página renderiza corretamente os elementos ', () => {
+    renderWithRouter(<NotFound />);
+
+    const title = screen.getByText('Page requested not found');
+    const image = screen.getByRole('img');
+    const URL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(title).toBeInTheDocument();
+    expect(image).toHaveAttribute('src', URL);
+  });
+});
